6ca018f894395967def3cb08f05df40475703dbc
russell-cmyk <russell@creativestartupnz.com>
2026-05-18 11:28:05 +1200

    Refactor header into custom SiteHeader element

---
+++ header.js
@@ -0,0 +1,30 @@
+class SiteHeader extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+        <header class="control-bar">
+            <div class="container header-content">
+                <a href="index.html" class="logo-area">
+                    <span>Take Care Property Maintenance</span>
+                </a>
+                
+                <nav class="main-nav">
+                    <ul>
+                        <li><a href="index.html">Home</a></li>
+                        <li><a href="index.html#services">Services</a></li>
+                        <li><a href="index.html#about">About</a></li>
+                        <li><a href="contact.html">Contact</a></li>
+                    </ul>
+                </nav>
+
+                <div class="header-cta">
+                    <a href="tel:0273361513" class="btn-primary">Call 027 336 1513</a>
+                </div>
+                <div class="mobile-toggle">☰</div>
+            </div>
+        </header>
+        `;
+    }
+}
+
+// Register the custom element
+customElements.define('site-header', SiteHeader);

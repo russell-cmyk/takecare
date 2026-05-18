ec00f7f7216602d0ae80fe525b2a5419eb1aa5c5
russell-cmyk <russell@creativestartupnz.com>
2026-05-18 13:27:27 +1200

    Update navigation links in header.js

--- header.js
+++ header.js
@@ -10,8 +10,9 @@ class SiteHeader extends HTMLElement {
                 <nav class="main-nav">
                     <ul>
                         <li><a href="index.html">Home</a></li>
-                        <li><a href="index.html#services">Services</a></li>
-                        <li><a href="index.html#about">About</a></li>
+                        <li><a href="services.html">Services</a></li>
+                        <li><a href="about.html">About</a></li>
+                        <li><a href="blog.html">Advice</a></li>
                         <li><a href="contact.html">Contact</a></li>
                     </ul>
                 </nav>
